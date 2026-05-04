ff3e0bde70200bff9f9d3bdd38e993fb63b3dfb5
gaganjogi <gagan.r@talview.com>
2026-05-05 01:11:51 +0530

    daily temperatures monotanic stack

--- Array/hashmap_hashset.js
+++ Array/hashmap_hashset.js
@@ -123,4 +123,17 @@ function groupAnagram(arr){
         return -1
     }
 
-    console.log('commit')
+    var dailyTemperatures = function(temperatures) {
+        const stack=[]
+        const result=new Array(temperatures.length).fill(0) 
+    
+        for(let i=0;i<temperatures.length;i++){
+    
+            while(stack.length>0 && temperatures[i]>temperatures[stack[stack.length-1]]){
+                 const index= stack.pop()
+                  result[index]=i-index
+            }
+            stack.push(i)
+        }
+        return result
+    };

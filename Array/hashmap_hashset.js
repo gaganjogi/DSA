a87daeaa31428c1d5e0ecc0bbc691c21c9f5bbac
gaganjogi <gagan.r@talview.com>
2026-05-15 01:51:40 +0530

    check linked list is cycle

--- Array/hashmap_hashset.js
+++ Array/hashmap_hashset.js
@@ -257,3 +257,17 @@ function groupAnagram(arr){
             }
             return longest_max_length
         };
+
+        var hasCycle = function(head) {
+            let slow=head
+            let fast=head
+        
+            while(fast!==null && fast.next!==null){
+                slow=slow.next
+                fast=fast.next.next
+        
+                if(slow==fast) return true
+            }
+        
+            return false
+        };

17ded9625282d4ef503265a3a23a304c709a7542
gaganjogi <gagan.r@talview.com>
2026-05-26 23:18:33 +0530

    has cycle in linked list

--- Array/hashmap_hashset.js
+++ Array/hashmap_hashset.js
@@ -315,3 +315,15 @@ function groupAnagram(arr){
        return newHead
     };
     
+    var hasCycle = function(head) {
+        let slow=head
+        let fast=head
+        while(fast!=null && fast.next!=null){
+            slow=slow.next
+            fast=fast.next.next
+    
+            if (slow==fast) return true
+        }
+    
+        return false
+    };

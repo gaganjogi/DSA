38fc71f903273977ccae52ffa8c9e377439e7384
GAGAN R <gaganr@192.168.1.10>
2026-07-19 00:09:02 +0530

    factory pattern example

--- LLD/design-pattern/factory-design.ts
+++ LLD/design-pattern/factory-design.ts
@@ -9,6 +9,9 @@
 interface Burger {
     prepare(): void;
 }
+interface GarlicBread {
+    prepare(): void;
+}
 class ChickenBurger implements Burger {
     prepare(): void {
         console.log("Chicken Burger");
@@ -36,6 +39,18 @@ class SupremeBurger implements Burger {
     }
 }
 
+class BasicGarlicBread implements GarlicBread {
+    prepare(): void {
+        console.log("Basic Garlic Bread");
+    }
+}
+class CheeseGarlicBread implements GarlicBread {
+    prepare(): void {
+        console.log("Cheese Garlic Bread");
+    }
+}
+
+
 // class BurgerFactory{
 //     createBurger(type:string):Burger {
 //          switch(type){
@@ -57,6 +72,7 @@ class SupremeBurger implements Burger {
 
 interface BurgerFactory {
     createBurger(type:string):Burger;
+    createGarlicBread(type:string):GarlicBread;
 }
 
 class SinghBurgerFactory implements BurgerFactory {
@@ -71,6 +87,15 @@ class SinghBurgerFactory implements BurgerFactory {
         }
         throw new Error("Invalid burger type");
     } 
+    createGarlicBread(type:string): GarlicBread {
+        switch(type) {
+            case "basic":
+                return new BasicGarlicBread();
+            case "cheese":
+                return new CheeseGarlicBread();
+        }
+        throw new Error("Invalid garlic bread type");
+    }
 }
 class ChawlaBurgerFactory implements BurgerFactory {
     createBurger(type:string): Burger{
@@ -82,7 +107,18 @@ class ChawlaBurgerFactory implements BurgerFactory {
         }
         throw new Error("Invalid burger type");
     } 
+    createGarlicBread(type:string): GarlicBread {
+        switch(type) {
+            case "basic":
+                return new BasicGarlicBread();
+            case "cheese":
+                return new CheeseGarlicBread();
+        }
+        throw new Error("Invalid garlic bread type");
+    }
 }
+
+
 
 console.log("Starting execution...");
 
@@ -103,3 +139,11 @@ console.log("Execution complete!");
 
 //factory method pattern - defines an interface for creating objects, but lets subclasses decide which class to instantiate
 
+
+//abstract factory pattern - provides an interface for creating families of related or dependent objects without specifying their concrete classes
+
+//which one to use when?
+// algorithmic approach:
+// - if you need to create ONE object at a time -> use factory method
+// - if you need to create MULTIPLE related objects at once -> use abstract factory
+// - if u need to run algorithm at the run time - use strategy pattern

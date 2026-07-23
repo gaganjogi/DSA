71412ef895f7fcd32a3b4866cb90d2cc23835811
gaganjogi <gagan8055r@gmail.com>
2026-07-23 12:38:31 +0530

    tree representation

---
+++ Trees/tree-representation.js
@@ -0,0 +1,15 @@
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+
+console.log(root);

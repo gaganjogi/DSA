9d351a7ad4c578118aecba479807218d2f9c5392
gaganjogi <gagan8055r@gmail.com>
2026-07-29 19:35:40 +0530

    delete operation in linkedlist

--- Linked List/js/linkedlist.js
+++ Linked List/js/linkedlist.js
@@ -41,6 +41,66 @@ return true
         return false
     }
 
+   deleteHead(){
+let op=this.head
+
+    op=this.head.next
+    this.head=null
+    this.head=op
+    return this
+
+
+}
+
+
+
+
+
+deleteTail()
+{
+    let tail=this.head
+ 
+if(tail==null || tail.next===null) 
+{
+    this.head=null
+    return null
+}
+
+    while(tail.next.next!=null){
+        tail=tail.next
+    }
+
+    tail.next=null
+
+return this
+}
+
+deleteKthElement(k)
+{
+    let element=this.head
+    let prev_element=null
+    if(element==null)
+    {
+        return null
+    }
+    if(k==1) {
+        return this.deleteHead()
+        }
+
+       for(let i=1;i<k;i++)
+       {
+
+       prev_element=element
+        element=element.next
+       }
+       prev_element.next=element.next
+       element.next=null
+ return this
+
+}
+
+
+
 
 }
 
